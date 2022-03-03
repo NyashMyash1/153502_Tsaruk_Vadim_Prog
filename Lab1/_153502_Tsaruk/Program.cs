afe6cd15f7c2bbfbad092e26d9078360133f6aa6
Вадим Царук <15350141@study.bsuir.by>
2022-03-03 12:23:01 +0300

    Add project files.

---
+++ Lab1/_153502_Tsaruk/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите делимое: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите делитель: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a / b;
+Console.WriteLine($"Частное: {c}");
