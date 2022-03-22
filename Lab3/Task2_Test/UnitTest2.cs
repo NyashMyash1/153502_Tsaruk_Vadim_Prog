cfeabf42ddfa61306c02c1dd99b8ee09d5a810f2
Вадим Царук <15350141@study.bsuir.by>
2022-03-22 09:06:28 +0300

    Lab3 fixed

--- Lab3/Task2_Test/UnitTest2.cs
+++ Lab3/Task2_Test/UnitTest2.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Task2.Services;
 
 namespace Task2_Test
 {
@@ -9,6 +10,41 @@ namespace Task2_Test
         [TestMethod]
         public void TestMethod1()
         {
+            Solution method = new Solution();
+            string test = String.Format("{0:f5}", method.Sol_Test(1));
+            Assert.AreEqual("2.75587", test);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution method = new Solution();
+            string test = String.Format("{0:f5}", method.Sol_Test(-9));
+            Assert.AreEqual("21.28851", test);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution method = new Solution();
+            string test = String.Format("{0:f5}", method.Sol_Test(-15));
+            Assert.AreEqual("150.11117", test);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution method = new Solution();
+            string test = String.Format("{0:f5}", method.Sol_Test(10));
+            Assert.AreEqual("22028.81712", test);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution method = new Solution();
+            string test = String.Format("{0:f5}", method.Sol_Test(7));
+            Assert.AreEqual("1098.64715", test);
         }
     }
 }
