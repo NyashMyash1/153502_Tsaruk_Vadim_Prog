04166d52336094eaa68a255b5b75e0d72aae0194
Вадим Царук <15350141@study.bsuir.by>
2022-03-24 13:01:45 +0300

    Lab4 added

---
+++ Lab4/Task1/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATS mts = ATS.GetInstanceATS();
+            mts.AdressATS = "Минск";
+            mts.Clients = 1000;
+            mts.ATSTariffOn();
+            mts.TariffCostUp(1.5);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Console.WriteLine(mts.AdressATS);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Tariff
+    {
+        //текущий тариф
+        private double currentTariff;
+        public double CurrentTariff
+        {
+            get { return currentTariff; }
+        }
+
+        //состояние тарифа 
+        private bool isTariffOn;
+        public bool IsTariffOn
+        {
+            get { return isTariffOn; }
+        }
+
+        //конструкторы 
+        public Tariff()
+        {
+            isTariffOn = true;
+            currentTariff = 10;
+        }
+
+        public Tariff(double tariff)
+        {
+            isTariffOn = true;
+            currentTariff = tariff;
+        }
+
+        public Tariff(bool isOnOff, double tariff)
+        {
+            isTariffOn = isOnOff;
+            if (!isTariffOn)
+                currentTariff = 0;
+            else 
+                currentTariff = tariff;
+        }
+
+        //включить тариф
+        public void TariffOn()
+        {
+            isTariffOn = true;
+        }
+        public void TariffOn(double tariff)
+        {
+            isTariffOn = true;
+            currentTariff = tariff;
+        }
+        //выключить тариф
+        public void TariffOff()
+        {
+            isTariffOn = false;
+            currentTariff = 0;
+        }
+        //увеличить тариф
+        public void TariffUp(double x)
+        {
+            if (isTariffOn)
+                currentTariff += x;
+        }
+        //уменьшить тариф
+        public void TariffDown(double x)
+        {
+            currentTariff -= x;
+        }
+    }
+
+    public class ATS
+    {
+        private static ATS atsInstance;
+        private Tariff atsTariff;
+        //закрытый конструктор 
+        private ATS()
+        {
+           atsTariff = new Tariff(false, 0);    
+        }
+        //реализация шаблона singleton
+        public static ATS GetInstanceATS()
+        {
+            if (atsInstance == null)
+                atsInstance = new ATS();
+            return atsInstance;
+        }
+        //название АТС
+        private string adressATS;
+        public string AdressATS
+        {
+            get{ return adressATS; }
+            set{ adressATS = value; }
+        }
+        //состояние тарифа 
+        public bool IsATSTariffOn
+        {
+            get { return atsTariff.IsTariffOn; }
+        }
+        public void ATSTariffOn()
+        {
+            atsTariff.TariffOn();
+        }
+
+        public void ATSTariffOn(double x)
+        {
+            atsTariff.TariffOn(x);
+        }
+
+        public void ATSTariffOff()
+        {
+            atsTariff.TariffOff();
+        }
+        //текущая плата по тарифу
+        public double ATSCurrentTariff
+        {
+            get { return atsTariff.CurrentTariff; }
+        }
+        //увеличить тариф 
+        public void TariffCostUp(double x)
+        {
+            atsTariff.TariffUp(x);
+        }
+        //уменьшить тариф 
+        public void TariffCostDown(double x)
+        {
+            atsTariff.TariffDown(x);
+        }
+        //абонентская плата за кол-во дней
+        private double subsFee;
+        public double SubsFee
+        {
+            get { return subsFee; }
+            set { subsFee = value * ATSCurrentTariff; }
+        }
+        //число клиентов
+        private int clients;
+        public int Clients
+        {
+            get { return clients; }
+            set { clients = MaxClients(value); }
+        }
+        //абонентская плата для всех клиентов 
+        public double AllSubsFee()
+        {
+            return clients * subsFee;
+        }
+
+        //статический метод
+        static int maxClients = 100000;
+        public static int MaxClients(int x)
+        {
+            if(x > maxClients)
+            {
+                Console.WriteLine("Вы не можете обслуживать больше 100000 клиентов");
+                return maxClients;
+            }
+            else 
+                return x;
+        }
+    }
+}
---
+++ Lab4/Task1_Test/UnitTest1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Task1;
+
+namespace Task1_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ATS mts = ATS.GetInstanceATS();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            mts.Clients = 1000;
+            mts.ATSTariffOn();
+            mts.TariffCostUp(1.5);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(45000, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            mts.Clients = 500;
+            mts.TariffCostUp(1);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(37500, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            mts.Clients = 100;
+            mts.TariffCostDown(2);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(1500, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            mts.Clients = 1;
+            mts.TariffCostUp(0.5);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(30, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            mts.Clients = 10000;
+            mts.TariffCostDown(0.5);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(150000, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            mts.Clients = 100;
+            mts.TariffCostUp(0.4);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(2700, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            mts.Clients = 10;
+            mts.TariffCostUp(1.1);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(600, result);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            mts.Clients = 1000;
+            mts.TariffCostDown(2);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            mts.Clients = 100;
+            mts.TariffCostUp(0.1);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(300, result);
+        }
+
+        [TestMethod]
+        public void TestMethodSubsFee()
+        {
+            mts.Clients = 10000;
+            mts.TariffCostUp(0.4);
+            mts.SubsFee = 30;
+            double result = mts.AllSubsFee();
+            Assert.AreEqual(150000, result);
+        }
+    }
+}
